438ddfd9566f4549031b7921f1e7d232a4fa393d
Amir shiri <plus1319@outlook.com>
2020-01-16 17:09:47 +0330

    initial rabbitmq

---
+++ Producer/Sender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                    string publishMessage = "Getting started with .net core";
+                    byte[] body = Encoding.UTF8.GetBytes(publishMessage);
+                        
+                    channel.BasicPublish("","BasicTest",null,body);
+                    Console.WriteLine($"Sending {publishMessage}");
+                }
+
+            Console.ReadKey();
+        }
+    }
+}
